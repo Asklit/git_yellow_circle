de75c192d3284d4272ff90e6d5771a9caf0fdcde
asklit <forgrace5@gmail.com>
2021-11-23 18:29:29 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import sys
+import random
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui.ui', self)
+        self.do_paint = False
+        self.btn_create.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_ellipse(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_ellipse(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        radius = random.randrange(1, 150)
+        qp.drawEllipse(200 - radius // 2, 200 - radius // 2, radius, radius)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
